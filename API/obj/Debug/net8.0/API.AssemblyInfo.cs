f67c2b9fcb383cf7d92c0710c992bb7546bfde70
Petersonwei <140482566+Petersonwei@users.noreply.github.com>
2024-11-12 22:20:29 +1000

    styled shopping items

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f4e5d9fea72fe9ad77a8dd9a5efcfb03c40ef3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ed0d74ee8c778b76cf514772ed856347b569fd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
