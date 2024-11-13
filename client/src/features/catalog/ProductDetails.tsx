e2c8206b77c1b051f71bb991219979f44a918388
Petersonwei <140482566+Petersonwei@users.noreply.github.com>
2024-11-13 13:28:09 +1000

    end of section 4

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Divider, Grid, Tab, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -17,12 +17,41 @@ export default function ProductDetails() {
     }, [id])  // id is the dependency here that will trigger the effect to re-run every time it changes
 
     if (loading) return <h3>Loading...</h3>
-    
+
     if (!product) return <h3>Product not found</h3>
 
     return (
-    <Typography variant="h2">
-        {product.name}
-    </Typography>
+        <Grid container spacing={6}>
+            <Grid item xs={6}>
+                <img src={product.pictureUrl} alt={product.name} style={{width: '100%'}} />
+            </Grid>
+            <Grid item xs={6}>
+                <Typography variant="h3">{product.name}</Typography>
+                <Divider sx={{mb: 2}} />
+                <Typography variant="h4" color="secondary">${(product.price / 100).toFixed(2)}</Typography>
+                <TableContainer>
+                    <Table>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>{product.name}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Description</TableCell>
+                                <TableCell>{product.description}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Brand</TableCell>
+                                <TableCell>{product.brand}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Quantity in stock</TableCell>
+                                <TableCell>{product.quantityInStock}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Grid>
+        </Grid>
     )
 }
